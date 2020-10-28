99c9bf2de5bd1d7a29bd6bfe178d56946ee3602e
Henry Zhao <zhaohj126@126.com>
2020-10-28 20:58:03 +0800

    udpate sarsa

--- cliff_sarsa.py
+++ cliff_sarsa.py
@@ -17,16 +17,18 @@
 # -*- coding: utf-8 -*-
 
 import gym
-from gridworld import CliffWalkingWapper, FrozenLakeWapper
+from gridworld import CliffWalkingWapper
+#from gridworld import FrozenLakeWapper
 import sarsa
 import time
 
-
-def run_episode(env, agent, render=False):
+# episode training
+def train_episode(env, agent, render=False):
     total_steps = 0  # 记录每个episode走了多少step
     total_reward = 0
 
     obs = env.reset()  # 重置环境, 重新开一局（即开始新的一个episode）
+                        # obs: initial state
     action = agent.sample(obs)  # 根据算法选择一个动作
 
     while True:
@@ -36,13 +38,16 @@ def run_episode(env, agent, render=False):
         agent.learn(obs, action, reward, next_obs, next_action, done)
 
         action = next_action
-        obs = next_obs  # 存储上一个观察值
+        obs = next_obs  # update current state and action
+
         total_reward += reward
         total_steps += 1  # 计算step数
+
         if render:
             env.render()  #渲染新的一帧图形
-        if done:
-            break
+        if done: 
+            break # reach terminal state, end the learning of this episode
+
     return total_reward, total_steps
 
 
@@ -50,12 +55,17 @@ def test_episode(env, agent):
     total_reward = 0
     obs = env.reset()
     while True:
-        action = agent.predict(obs)  # greedy
+        action = agent.predict(obs)  # greedy, no epsilon randomness
         next_obs, reward, done, _ = env.step(action)
-        total_reward += reward
+
+        # update state and reward
         obs = next_obs
-        time.sleep(0.5)
+        total_reward += reward
+
+        time.sleep(0.5) # for visualization
         env.render()
+
+        # reach terminal state
         if done:
             print('test reward = %.1f' % (total_reward))
             break
@@ -65,27 +75,29 @@ def main():
     # env = gym.make("FrozenLake-v0", is_slippery=False)  # 0 left, 1 down, 2 right, 3 up
     # env = FrozenLakeWapper(env)
 
+    # setup env
     env = gym.make("CliffWalking-v0")  # 0 up, 1 right, 2 down, 3 left
     env = CliffWalkingWapper(env)
 
-    agent = SarsaAgent(
-        obs_n=env.observation_space.n,
-        act_n=env.action_space.n,
+    # generate agent
+    agent = sarsa.SarsaAgent(
+        obs_n=env.observation_space.n, # the dimension of the state space of env
+        act_n=env.action_space.n, # the dimension of the action space of env
         learning_rate=0.1,
         gamma=0.9,
         e_greed=0.1)
 
     is_render = False
-    for episode in range(500):
-        ep_reward, ep_steps = run_episode(env, agent, is_render)
-        print('Episode %s: steps = %s , reward = %.1f' % (episode, ep_steps,
-                                                          ep_reward))
+    for episode in range(500): # learn 500 episode
+        ep_reward, ep_steps = train_episode(env, agent, is_render)
+        print('Episode %s: steps = %s , reward = %.1f' % (episode, ep_steps, ep_reward))
 
         # 每隔20个episode渲染一下看看效果
         if episode % 20 == 0:
             is_render = True
         else:
             is_render = False
+
     # 训练结束，查看算法效果
     test_episode(env, agent)
 
